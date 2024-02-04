28a65f7078db3754fe7fda606b2306386b63aa06
kloeffler95 <kevin.loeffler@colorado.edu>
2024-02-03 21:24:09 -0700

    Cleanup.

--- carboncli.py
+++ carboncli.py
@@ -40,15 +40,12 @@ def main():
         else:
             carbon = CarbonCalculator(lighthouse=lighthouse, reqs=10000)
         carbon.footprint(website)
-
+        f = open("badge.svg", "w")
         if carbon.hosting_green:
-            f = open("badge.svg", "w")
             f.write('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="130" height="20" role="img" aria-label="Green Webhost: True"><title>Green Webhost: True</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="130" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="95" height="20" fill="#555"/><rect x="95" width="35" height="20" fill="#97ca00"/><rect width="130" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="485" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="850">Green Webhost</text><text x="485" y="140" transform="scale(.1)" fill="#fff" textLength="850">Green Webhost</text><text aria-hidden="true" x="1115" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="250">True</text><text x="1115" y="140" transform="scale(.1)" fill="#fff" textLength="250">True</text></g></svg>')
-            f.close()
         else:
-            f = open("badge.svg", "w")
             f.write('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="134" height="20" role="img" aria-label="Green Webhost: False"><title>Green Webhost: False</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="134" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="95" height="20" fill="#555"/><rect x="95" width="39" height="20" fill="#e05d44"/><rect width="134" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="485" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="850">Green Webhost</text><text x="485" y="140" transform="scale(.1)" fill="#fff" textLength="850">Green Webhost</text><text aria-hidden="true" x="1135" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="290">False</text><text x="1135" y="140" transform="scale(.1)" fill="#fff" textLength="290">False</text></g></svg>')
-            f.close()
+        f.close()
         print(carbon.to_json())
     except Exception as e:
         print(e)
